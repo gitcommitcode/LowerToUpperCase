99b539b516453f43e446b6adaa883604fd36d591
gitcommitcode <rahulkmr5041@gmail.com>
2023-04-24 14:53:46 +0530

    added dark mode option

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react';
 function App() {
+  const [mode,setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey'
+    }
+    else if(mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
    <>
-    <Navbar title = "TextUtils"/>
+    <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className="container">
-      <TextForm></TextForm>
+      <TextForm mode={mode}></TextForm>
     </div>
    </>
   );
